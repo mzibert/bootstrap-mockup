4e8b3f543fbb6eaddf3ecf1cd5a045a8ddac773c
merri zibert <mzibert@cnm.edu>
2016-05-08 22:20:56 -0600

    bootstrap mockup index page

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"></html>
+<head>
+	<meta charset="utf-8">
+
+	<!---sets IE rendering to IE Edge -->
+	<meta http-equiv="X-UA-COMPATIBLE" content="IE=Edge"/>
+
+	<!---sets viewport width to device width, scaling 1:1 -->
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+			integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!--- inputting FontAwesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+	<!---HTML5 conditional comment for any IE 6-9 users -->
+	<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+	<!---Our CSS file -->
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+	<!---JQuery -->
+	<script src="https://code.jquery.com/jquery-2.2.3.min.js"
+			  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+			  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+			  crossorigin="anonymous"></script>
+
+	<title>Hello Bootstrap</title>
+</head>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<header>
+			<div class="contianer">
+				<!---begin bootstrap nav bar-->
+				<!---brand and toggle grouped for better mobile display-->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+							  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Brand</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+						<li><a href="#">Link</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								aria-expanded="false">Dropdown <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="http://google.com/">google.com</a></li>
+								<li><a href="https://www.ted.com/talks">Get Inspired</a></li>
+								<li><a href="https://www.youtube.com/vlogbrothers">Nerdfighteria</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+
+		</header>
+		<main>
+
+
+		</main>
+		<div class="container>"
+		<div class="row">
+
+			<!---Welcome Section-->
+			<div class="col-xs-12 well text-center">
+				<h1>Welcome to my website! YAY!</h1>
+				<p>Some text down here!</p>
+			</div>
+			<!---first content section-->
+			<div class=row>
+				<div class="col-md-3">
+					<div class="well">
+						<h4>Section 1</h4>
+						<p>Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles
+							banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12
+							inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn
+							blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices
+							sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears
+							good clean match.</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="well">
+						<h4>Section 2</h4>
+						<p>Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of
+							silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do
+							it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he
+							will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless
+							carriages ‘zis is a chance many would die for!</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="well">
+						<h4>Section 3</h4>
+						<p>Prefect’s bathroom Trelawney veela squashy armchairs, SPEW: Gamp’s Elemental Law of
+							Transfiguration. Magic Nagini bezoar, Hippogriffs Headless Hunt giant squid petrified. Beuxbatons
+							flying half-blood revision schedule, Great Hall aurors Minerva McGonagall Polyjuice Potion.
+							Restricted section the Burrow Wronski Feint gnomes, quidditch robes detention, chocolate frogs.</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="well">
+						<h4>Section 4</h4>
+						<p>Errol parchment knickerbocker glory Avada Kedavra Shell Cottage beaded bag portrait vulture-hat.
+							Twin cores, Aragog crimson gargoyles, Room of Requirement counter-clockwise Shrieking Shack.
+							Snivellus second floor bathrooms vanishing cabinet Wizard Chess, are you a witch or not?</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="well">
+						<h4>Section 5</h4>
+						<p>Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles
+							banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12
+							inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn
+							blood crossbow mars is bright tonight, feast Norwegian Ridgeback.</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="well">
+						<h4>Section 6</h4>
+					</div>
+				</div>
+			</div>
+		</div><!--/.row-->
+	</div><!--/.container-->
+	</main>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="foot-content"
+						  content here
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="copyright text-center">
+					&copy; 2016
+				</div>
+			</div>
+		</div>
+		</div>
+
+	</footer>
+</body>
+</html>
